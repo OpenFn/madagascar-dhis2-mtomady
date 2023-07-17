1f00988d5e4c46fbd1d94b2cd227505aee6e7386
Taylor Downs <taylor@openfn.org>
2023-07-17 10:00:04 -0600

    taylor@openfn.org's change on OpenFn.org

--- jobs/load-teis.js
+++ jobs/load-teis.js
@@ -5,11 +5,11 @@ fn(state => {
   const treatmentName = `${vaccine.display} (${vaccine.code})`;
   const now = new Date();
   const today = now.toISOString().split('T')[0];
-
+  
   console.log('patient', patient);
   console.log('claim', claim);
   console.log('treatmentName', treatmentName);
-
+  
   return { ...state, treatmentName, today };
 });
 
@@ -19,25 +19,25 @@ create('trackedEntityInstances', {
   trackedEntityType: 'x5fZpgCyv50', // Patient
   attributes: state => [
     {
-      attribute: 'E4f4wBsDVgR',
-      value: state.patient.name.family,
+      "attribute": "E4f4wBsDVgR",
+      "value": state.patient.name.family
     },
     {
-      attribute: 'Fz33peSkK1I',
-      value: state.patient.name[0].given[0],
+      "attribute": "Fz33peSkK1I",
+      "value": state.patient.name[0].given[0]
     },
     {
-      attribute: 'POCXiJxpYX1',
-      value: state.treatmentName, // "MUMPS VACCINE (871737006)"
+      "attribute": "POCXiJxpYX1",
+      "value": state.treatmentName // "MUMPS VACCINE (871737006)"
     },
     {
-      attribute: 'dA6ShmrHmhk',
-      value: state.patient.birthDate,
+      "attribute": "dA6ShmrHmhk",
+      "value": state.patient.birthDate
     },
     {
-      attribute: 'mWOlfweGigO',
-      value: state.patient.gender,
-    },
+      "attribute": "mWOlfweGigO",
+      "value": state.patient.gender
+    }
   ],
   enrollments: [
     {
@@ -45,7 +45,7 @@ create('trackedEntityInstances', {
       program: 'GMfuAqBFS1g',
       status: 'ACTIVE', // active
       enrollmentDate: state.today,
-      incidentDate: state.today,
+      incidentDate: state.today
     },
   ],
 });
