3440b19c5d39911110988c213f07e01637c89a1a
Taylor Downs <downs.taylor@gmail.com>
2023-07-17 09:59:48 -0600

    concept 1

---
+++ jobs/fetch-claims.js
@@ -0,0 +1,23 @@
+// operation 1 - grab claim data
+get(state => `Claim/${state.data.claimNo}/_history/2`, {
+  query: { _pretty: true },
+  headers: {'content-type': 'application/json'}
+});
+
+// custom function to see what we get!
+fn(state => {
+ console.log(JSON.stringify(state.data, null, 2));
+ return { ...state, claim: state.data };
+})
+
+// Now, get the patient which is referenced in the claim
+get(state => `${state.claim.patient.reference}/_history/2`, {
+  query: { _pretty: true },
+  headers: {'content-type': 'application/json'}
+});
+
+// log the patient
+fn(state => {
+ console.log(JSON.stringify(state.data, null, 2));
+ return { ...state, patient: state.data };
+})
---
+++ jobs/load-teis.js
@@ -0,0 +1,51 @@
+fn(state => {
+  const { patient, claim } = state;
+
+  const vaccine = claim.item[0].productOrService.coding[0];
+  const treatmentName = `${vaccine.display} (${vaccine.code})`;
+  const now = new Date();
+  const today = now.toISOString().split('T')[0];
+
+  console.log('patient', patient);
+  console.log('claim', claim);
+  console.log('treatmentName', treatmentName);
+
+  return { ...state, treatmentName, today };
+});
+
+create('trackedEntityInstances', {
+  program: 'GMfuAqBFS1g',
+  orgUnit: 'KUVJPjmUmWc', // Madagascar
+  trackedEntityType: 'x5fZpgCyv50', // Patient
+  attributes: state => [
+    {
+      attribute: 'E4f4wBsDVgR',
+      value: state.patient.name.family,
+    },
+    {
+      attribute: 'Fz33peSkK1I',
+      value: state.patient.name[0].given[0],
+    },
+    {
+      attribute: 'POCXiJxpYX1',
+      value: state.treatmentName, // "MUMPS VACCINE (871737006)"
+    },
+    {
+      attribute: 'dA6ShmrHmhk',
+      value: state.patient.birthDate,
+    },
+    {
+      attribute: 'mWOlfweGigO',
+      value: state.patient.gender,
+    },
+  ],
+  enrollments: [
+    {
+      orgUnit: 'KUVJPjmUmWc',
+      program: 'GMfuAqBFS1g',
+      status: 'ACTIVE', // active
+      enrollmentDate: state.today,
+      incidentDate: state.today,
+    },
+  ],
+});
