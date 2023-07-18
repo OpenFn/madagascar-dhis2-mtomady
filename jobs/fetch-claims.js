8886c2b74f630e914b039c8dfb81824e639f4788
Taylor Downs <downs.taylor@gmail.com>
2023-07-18 06:47:27 -0600

    logging, comment out load for now

--- jobs/fetch-claims.js
+++ jobs/fetch-claims.js
@@ -1,13 +1,13 @@
 // Get claim data based on webhook payload
 get(
-  state => `Claim/${state.data.claimNo}/_history/2`,
+  state => `Claim/${state.data.claimNo}`,
   {},
   next => ({ ...next, claim: next.data })
 );
 
 // Now, get the patient which is referenced in the claim
 get(
-  state => `${state.claim.patient.reference}/_history/2`,
+  state => `${state.claim.patient.reference}`,
   {},
   next => ({ ...next, patient: next.data })
 );
--- jobs/load-teis.js
+++ jobs/load-teis.js
@@ -1,51 +1,38 @@
+// build and log nested dhis2 records
 fn(state => {
   const { patient, claim } = state;
 
+  // Create "treatmentName" by concatenating...
   const vaccine = claim.item[0].productOrService.coding[0];
   const treatmentName = `${vaccine.display} (${vaccine.code})`;
-  const now = new Date();
-  const today = now.toISOString().split('T')[0];
 
-  console.log('patient', patient);
-  console.log('claim', claim);
-  console.log('treatmentName', treatmentName);
+  const today = new Date().toISOString().split('T')[0];
 
-  return { ...state, treatmentName, today };
+  const tei = {
+    program: 'GMfuAqBFS1g', // Vaccination
+    orgUnit: 'KUVJPjmUmWc', // Madagascar
+    trackedEntityType: 'x5fZpgCyv50', // Patient
+    attributes: [
+      { attribute: 'E4f4wBsDVgR', value: patient.name[0].family },
+      { attribute: 'Fz33peSkK1I', value: patient.name[0].given[0] },
+      { attribute: 'POCXiJxpYX1', value: treatmentName },
+      { attribute: 'dA6ShmrHmhk', value: patient.birthDate },
+      { attribute: 'mWOlfweGigO', value: patient.gender },
+    ],
+    enrollments: [
+      {
+        orgUnit: 'KUVJPjmUmWc',
+        program: 'GMfuAqBFS1g',
+        status: 'ACTIVE', // active
+        enrollmentDate: today,
+        incidentDate: today,
+      },
+    ],
+  };
+
+  console.log('tei', JSON.stringify(tei, null, 2));
+  return { ...state, tei };
 });
 
-create('trackedEntityInstances', {
-  program: 'GMfuAqBFS1g', // Vaccination
-  orgUnit: 'KUVJPjmUmWc', // Madagascar
-  trackedEntityType: 'x5fZpgCyv50', // Patient
-  attributes: state => [
-    {
-      attribute: 'E4f4wBsDVgR',
-      value: state.patient.name.family,
-    },
-    {
-      attribute: 'Fz33peSkK1I',
-      value: state.patient.name[0].given[0],
-    },
-    {
-      attribute: 'POCXiJxpYX1',
-      value: state.treatmentName, // "MUMPS VACCINE (871737006)"
-    },
-    {
-      attribute: 'dA6ShmrHmhk',
-      value: state.patient.birthDate,
-    },
-    {
-      attribute: 'mWOlfweGigO',
-      value: state.patient.gender,
-    },
-  ],
-  enrollments: [
-    {
-      orgUnit: 'KUVJPjmUmWc',
-      program: 'GMfuAqBFS1g',
-      status: 'ACTIVE', // active
-      enrollmentDate: state.today,
-      incidentDate: state.today,
-    },
-  ],
-});
+// send data to DHIS2
+// create('trackedEntityInstances', state => tei);
