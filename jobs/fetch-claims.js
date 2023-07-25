df452391941bf499370c3759cf4b6fa7e1887e4b
Taylor Downs <downs.taylor@gmail.com>
2023-07-25 12:42:27 +0100

    more data quality logs

--- jobs/fetch-claims.js
+++ jobs/fetch-claims.js
@@ -1,8 +1,10 @@
 // establish a baseline for the cursor
 fn(state => {
   const today = new Date().toISOString().split('T')[0];
-  console.log(`Fetching all claims updated on or after ${state.cursor || '2023-07-19'}`);
-  return { ...state, today }
+  console.log(
+    `Fetching all claims updated on or after ${state.cursor || '2023-07-19'}`
+  );
+  return { ...state, today };
 });
 
 // Get claims and related patients from HAPI, updated since cursor date
@@ -17,8 +19,9 @@ get(
     },
   },
   next => {
-    if (next.data.total == 0) return { ...next, data: { claims: [], patients: [] }}
-    
+    if (next.data.total == 0)
+      return { ...next, data: { claims: [], patients: [] } };
+
     const byType = next.data.entry.reduce((r, a) => {
       r[a.resource.resourceType] = r[a.resource.resourceType] || [];
       r[a.resource.resourceType].push(a);
@@ -38,14 +41,27 @@ get(
 // clean and merge data
 fn(state => {
   const { claims, patients } = state.data;
-  
+
+  // Noisy logs to help debug data quality issues
+  claims.forEach(c => {
+    const { resource } = c;
+    if (!resource.id) console.log('claim', resource.id, 'has no id');
+    if (!resource.item) {
+      console.log('claim', resource.id, 'has no item');
+    } else if (!resource.item[0].productOrService.coding) {
+      console.log('claim', resource.id, 'has no coding');
+    }
+
+    if (!resource.patient.reference.split('/')[1])
+      console.log('claim', resource.id, 'has no patient');
+  });
+
   const patientsWithClaims = patients
     // drop all patients without identifiers
     .filter(p => p.resource.identifier)
     .map(p => ({
       ...p,
       claims: claims.filter(c => {
-        // console.log(JSON.stringify(c, null, 2));
         return (
           // has item
           c.resource.item &&
@@ -63,12 +79,14 @@ fn(state => {
 // print some logs and update the cursor for next time
 fn(state => {
   const { data, today } = state;
-  
-  console.log("Found the following patients and claims in FHIR:")
-  console.log(JSON.stringify(data.patientsWithClaims.map(p => ({
-    patient: p.resource.id,
-    claims: p.claims.map(c => c.resource.id)
-  })), null, 2));
-  
+
+  console.log('Found the following patients and claims in FHIR:');
+  console.log(
+    data.patientsWithClaims.map(p => ({
+      patient: p.resource.id,
+      claims: p.claims.map(c => c.resource.id),
+    }))
+  );
+
   return { data: state.data, cursor: today };
 });
